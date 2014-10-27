f7802b3a84459ef134551b3a54bc8e7d69fc2b7e
Tucker <tucker@altiscale.com>
2014-10-27 16:59:52 -0700

    Revert "Add name"

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,8 @@
-name              "build-essential"
 maintainer        "Opscode, Inc."
 maintainer_email  "cookbooks@opscode.com"
 license           "Apache 2.0"
 description       "Installs C compiler / build tools"
-version           "1.0.3"
+version           "1.0.2"
 recipe            "build-essential", "Installs packages required for compiling C software from source."
 
 %w{ fedora redhat centos ubuntu debian amazon }.each do |os|
